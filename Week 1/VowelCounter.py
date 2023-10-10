d72c8e8f9b0afa38e4c0d7974a31e7184e2ea32d
Izabella Orozco <iao4909@nyu.edu>
2023-10-10 17:21:19 -0400

    vowels

--- Week 1/VowelCounter.py
+++ Week 1/VowelCounter.py
@@ -1,2 +1,13 @@
 # Question 5 
-# 
+# Write a function that takes a word as an argument and returns the number of vowels in the word
+
+word = input("Please input a word: ")
+
+vowelcount = 0
+vowels = ['a','e','i','o','u']
+
+for character in word:
+    if character in vowels:
+        vowelcount +=1 
+        
+print(vowelcount)
