8f6a52853a5c6461244d239f98d15aa9a68b3fd9
iao4909 <iao4909@nyu.edu>
2023-11-06 23:31:34 -0500

    q45

--- Week4/question4-5.py
+++ Week4/question4-5.py
@@ -6,3 +6,37 @@
 # TODO
 # 5. Now, create a scatter plot of two heatmap entries that appear to have negative correlation.
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+housing_csv = open('Week4/data/boston_housing_data.csv')
+housing = pd.read_csv(housing_csv)
+
+
+housing_correlations = housing.corr();
+
+# heatmap
+plt.figure(figsize=(8, 6))
+sns.heatmap(housing_correlations, vmin=-1, vmax=1)
+plt.title('Correlation Heatmap for Housing Data')
+plt.show()
+
+# scatterplot POS
+positive_correlation_columns = ['TAX', 'CRIM']
+plt.figure(figsize=(10, 8))
+sns.scatterplot(data=housing, x='TAX', y='CRIM')
+plt.xlabel('TAX')
+plt.ylabel('CRIM')
+plt.title('Scatter Plot with Positive Correlation')
+plt.show()
+
+# scatterplot NEG
+negative_correlation_columns = ['LSTAT', 'MEDV']
+plt.figure(figsize=(10, 8))
+sns.scatterplot(data=housing, x='LSTAT', y='MEDV')
+plt.xlabel('LSTAT')
+plt.ylabel('MEDV')
+plt.title('Scatter Plot with Negative Correlation')
+plt.show()
