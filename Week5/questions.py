dab267b7ef61a49e78b5c7e8b5c5b68f045b7d38
iao4909 <iao4909@nyu.edu>
2023-11-19 23:51:59 -0500

    new

---
+++ Week5/questions.py
@@ -0,0 +1,25 @@
+# ## Practice Questions
+# 1. How do you assess the statistical significance of an insight?
+# Use the p-value which tells the probability of an observation occurring by chance, given the null hypothesis 
+
+# 2. What is the Central Limit Theorem? Explain it. Why is it important?
+# The central limit theorum explains that in random and independent samples, the sample distribution of the means are normally distributed.
+# The sample size must be large and this is important because it allows us to make assumptions with our data.
+
+# 3. What is the statistical power?
+# Statistical power is the likelihood of a significance test incorrectly detecting an effect.
+
+# 4. How do you control for biases?
+# To avoid bias, use diverse data and tools that check for unfair results or problems. 
+# Make sure that different groups are represented fairly.
+# Null Hypothesis Significance Testing
+
+# 5. What are confounding variables?
+# Cofounding variable can mess up the results because they appear to show a connection with the resulting data but do not.
+
+# 6. What is A/B testing?
+# A/B Testing is when two versions of something are compared in a divided sample group. 
+# It helps businesses or developers choose the better of the two optuons.
+
+# 7. What are confidence intervals?
+# Confidence intervals are ranges around a measurement that shows how precise the measurement is based on the certainty and uncertainty of the sampling method. 
